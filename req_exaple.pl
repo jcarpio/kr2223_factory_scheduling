dd2e9961b4f0fe457eacf17fdcd9db90011e59f0
jcarpio <jose.carpio@gmail.com>
2023-05-08 12:33:42 +0200

    Create req_exaple.pl

---
+++ req_exaple.pl
@@ -0,0 +1,12 @@
+slots_per_week(120).
+
+slots_per_day(24).
+
+
+tool_task_worker_times('fresadora', fresar, alex, 20).
+
+
+
+worker_freeday(alex, 1).
+
+worker_day_availability(alex, [1,1,0,0,0,0,0]).
